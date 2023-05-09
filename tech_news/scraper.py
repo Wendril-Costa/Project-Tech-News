9e67621197655769027090dcc4bdaf6f2c6a5c04
Wendril-Costa <wendrild.c.n@gmail.com>
2023-05-09 02:39:41 -0300

    Requisito 1 feito

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,18 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    headers = {"user-agent": "Fake user-agent"}
+    time.sleep(1)
+    try:
+        response = requests.get(url, headers=headers, timeout=3)
+        if response.status_code == 200:
+            return response.text
+    except requests.exceptions.RequestException:
+        return None
+    return None
 
 
 # Requisito 2
