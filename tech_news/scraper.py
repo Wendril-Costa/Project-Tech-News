8fba0e97f209b4724baf0f80579465bc36802dad
Wendril-Costa <wendrild.c.n@gmail.com>
2023-05-09 03:37:11 -0300

    Requisito 4 - Feito

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -19,19 +19,36 @@ def fetch(url):
 # Requisito 2
 def scrape_updates(html_content):
     sel = Selector(html_content)
-    return [card.css('a::attr(href)').get() for card in sel.css('.post')[1:]]
+    return [card.css("a::attr(href)").get() for card in sel.css(".post")[1:]]
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
     sel = Selector(html_content)
-    next_page = sel.css('a.next::attr(href)').get()
+    next_page = sel.css("a.next::attr(href)").get()
     return next_page if next_page else None
 
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    sel = Selector(html_content)
+    data = {}
+    data["url"] = sel.css('link[rel="canonical"]::attr(href)').get()
+    data["title"] = sel.css("h1.entry-title::text").get().strip()
+    data["timestamp"] = sel.css("li.meta-date::text").get().strip()
+    data["writer"] = (
+        sel.css("li.meta-author span.author a::text").get().strip()
+    )
+    data["reading_time"] = int(
+        sel.css("li.meta-reading-time::text").get().split()[0]
+    )
+    data["summary"] = "".join(
+        sel.css(".entry-content > p:first-of-type ::text").getall()
+    ).strip()
+    data["category"] = (
+        sel.css(".category-style .label::text").get().strip()
+    )
+    return data
 
 
 # Requisito 5
