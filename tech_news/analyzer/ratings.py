a92952df341cc6f8d344dbe656a009f4849df733
Wendril-Costa <wendrild.c.n@gmail.com>
2023-05-09 08:18:47 -0300

    Requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,11 @@
-# Requisito 10
+from tech_news.database import get_collection
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    collection = get_collection()
+    cursor = collection.aggregate([
+        {"$group": {"_id": "$category", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1, "_id": 1}},
+        {"$limit": 5}
+    ])
+    return [category["_id"] for category in cursor]
